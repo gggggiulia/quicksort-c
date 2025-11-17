b6ea1b056f775c8d9be9ea19db4fd6a741693732
giulia <169063222+gggggiulia@users.noreply.github.com>
2025-11-17 10:55:57 -0300

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -56,7 +56,9 @@ int main() {
     int i, N, R = 1000;
     int elementos_lidos = ler_arq(vet_base, INPUT);
     
-    if (elementos_lidos == 0) return 1;
+    if (elementos_lidos == 0){
+        return 1;
+    }
 
     FILE *output = fopen(OUTPUT, "wt");
     if (output == NULL) {
